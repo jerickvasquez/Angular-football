b6f86e75daeb5d10230a664d3d9fc7ed453457e1
jerickvasquez <jorgeerick446@gmail.com>
2022-03-26 20:25:41 -0500

    Arreglo de proxy y angular interceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MyInterceptor } from './interceptor/interceptor.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-football';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  //Apenas carga el componente esto se ejecuta
+  ngOnInit() {
+    this.http.get('/conferences').subscribe(
+      (res) => {
+        console.log('Entro a la respuesta');
+        console.log(res);
+      },
+      (err) => {
+        console.log('Entro al error');
+        console.log(err);
+      }
+    );
+  }
 }
